3b44c13e11ea27d084e868e88ab83af5995d3dc8
arrown <tolnane2@naver.com>
2021-11-11 13:18:56 +0000

    testgit add globallocationtest.py

--- globallocationtest.py
+++ globallocationtest.py
@@ -1,11 +1,26 @@
 import math
 import time
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from pymavlink import mavutil
+print "connecting to Drone"
+Drone = connect("/dev/serial0", baudrate = 57600, wait_ready = True) # telemetry line to raspberry pi
+Drone.airspeed = 1
+Drone.groundspeed = 0.3
+time.sleep(1)
 
-lon = 127.07463
-lat = 37.55000
-nlon = 127.075593
-nlat = 37.550859
-rad = 6378137
+print "connecting to Rover"
+Rover = connect("/dev/", baudrate = 57600, wait_ready = True) #telemetry receiver
+Rover.groundspeed = 1
+time.sleep(1)
+
+# swarm code
+tmp = y
+while tmp == y:
+    lat = Drone.location.global_relative_frame.lat # drone's latitude
+    lon = Drone.location.global_relative_frame.lon # drone's longitude
+    nlat = Rover.location.global_relative_frame.lat # rover's latitude
+    nlon = Rover.location.global_relative_frame.lon # rover's longitude
+
 dnorth = (nlat-lat)*rad*math.pi/180
 deast = math.pi/180*(nlon-lon)*rad*math.cos(lat/180*math.pi)
 print dnorth,deast
---
+++ pykeytest.py
@@ -0,0 +1,8 @@
+import keyboard
+import time
+
+while True:
+    if keyboard.read_key() =="p":
+        print "you pressed p"
+        break
+    
