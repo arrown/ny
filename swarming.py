953630f3317add2f4a6e841c1a7731c5888aa560
arrown <tolnane2@naver.com>
2021-11-11 11:50:58 +0000

    update swarming

--- swarming.py
+++ swarming.py
@@ -3,10 +3,11 @@ import math
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 from pymavlink import mavutil
 print("connecting to Drone")
-Drone = connect("/dev/ttyAMA0", baudrate = 57600, wait_ready = True)
+Drone = connect("/dev/serial0", baudrate = 57600, wait_ready = True)
 Drone.airspeed = 1
 Drone.groundspeed = 0.3
+time.sleep(1)
 print("conncting to Rover")
 Rover = connect("/dev/", baudrate = 57600, wait_ready = True)
 Rover.groundspeed = 1
-time.slee(1)
+time.sleep(1)
