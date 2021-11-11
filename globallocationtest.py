17a09c5f27857450a2abd4d82e30f0a8ddb8e63d
arrown <80968603+arrown@users.noreply.github.com>
2021-11-11 23:39:05 +0900

    Update globallocationtest.py

--- globallocationtest.py
+++ globallocationtest.py
@@ -37,7 +37,7 @@ def arm_and_takeoff(aTargetAltitude):
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
     #  after Vehicle.simple_takeoff will execute immediately).
     while True:
-        print (" Altitude: ", Drone.location.global_relative_frame.alt)
+        print (" Altitude: ", drone.location.global_relative_frame.alt)
         #Break and return from function just below target altitude.
         if drone.location.global_relative_frame.alt>=aTargetAltitude*0.95:
             print ("Reached target altitude")
@@ -64,7 +64,7 @@ def get_location_metres(original_location, dNorth, dEast):
     return targetlocation;
 
 
-def get_distance_metres_drone(aLocation1, aLocation2):
+def get_distance_metres(aLocation1, aLocation2):
 
     dlat = aLocation2.lat - aLocation1.lat
     dlong = aLocation2.lon - aLocation1.lon
@@ -117,7 +117,7 @@ def goto_position_target_local_ned_rover(north, east, down):
     # send command to vehicle
     rover.send_mavlink(msg)
 
-def goto_rover(dNorth, dEast, gotoFunction=drone.simple_goto):
+def goto_rover(dNorth, dEast, gotoFunction=rover.simple_goto):
 
     currentLocation = rover.location.global_relative_frame
     targetLocation = get_location_metres(currentLocation, dNorth, dEast)
@@ -140,119 +140,7 @@ def goto_rover(dNorth, dEast, gotoFunction=drone.simple_goto):
     for x in range(0,duration):
         rover.send_mavlink(msg)
         time.sleep(1) 
-        
-def set_roi_drone(location):
 
-    # create the MAV_CMD_DO_SET_ROI command
-    msg = drone.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
-        0, #confirmation
-        0, 0, 0, 0, #params 1-4
-        location.lat,
-        location.lon,
-        location.alt
-        )
-    # send command to vehicle
-    drone.send_mavlink(msg)
-
-
-def get_location_metres_drone(original_location, dNorth, dEast):
-
-    earth_radius = 6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
-
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
-        
-    return targetlocation;
-
-
-def get_distance_metres_drone(aLocation1, aLocation2):
-
-    dlat = aLocation2.lat - aLocation1.lat
-    dlong = aLocation2.lon - aLocation1.lon
-    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
-
-
-def get_bearing_drone(aLocation1, aLocation2):
-    """
-    Returns the bearing between the two LocationGlobal objects passed as parameters.
-    This method is an approximation, and may not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """	
-    off_x = aLocation2.lon - aLocation1.lon
-    off_y = aLocation2.lat - aLocation1.lat
-    bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
-    if bearing < 0:
-        bearing += 360.00
-    return bearing;
-
-def goto_position_target_global_int_drone(aLocation):
-
-    msg = drone.message_factory.set_position_target_global_int_encode(
-        0,       # time_boot_ms (not used)
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
-        0b0000111111111000, # type_mask (only speeds enabled)
-        aLocation.lat*1e7, # lat_int - X Position in WGS84 frame in 1e7 * meters
-        aLocation.lon*1e7, # lon_int - Y Position in WGS84 frame in 1e7 * meters
-        aLocation.alt, # alt - Altitude in meters in AMSL altitude, not WGS84 if absolute or relative, above terrain if GLOBAL_TERRAIN_ALT_INT
-        0, # X velocity in NED frame in m/s
-        0, # Y velocity in NED frame in m/s
-        0, # Z velocity in NED frame in m/s
-        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
-        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
-    # send command to vehicle
-    drone.send_mavlink(msg)
-
-
-
-def goto_position_target_local_ned_drone(north, east, down):
-
-    msg = drone.message_factory.set_position_target_local_ned_encode(
-        0,       # time_boot_ms (not used)
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
-        0b0000111111111000, # type_mask (only positions enabled)
-        north, east, down, # x, y, z positions (or North, East, Down in the MAV_FRAME_BODY_NED frame
-        0, 0, 0, # x, y, z velocity in m/s  (not used)
-        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
-        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
-    # send command to vehicle
-    drone.send_mavlink(msg)
-
-
-
-def goto_rover(dNorth, dEast, gotoFunction=drone.simple_goto):
-
-    currentLocation = rover.location.global_relative_frame
-    targetLocation = get_location_metres(currentLocation, dNorth, dEast)
-    targetDistance = get_distance_metres(currentLocation, targetLocation)
-    gotoFunction(targetLocation)
-    
-    #print "DEBUG: targetLocation: %s" % targetLocation
-    #print "DEBUG: targetLocation: %s" % targetDistance
-
-    while rover.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
-        #print "DEBUG: mode: %s" % vehicle.mode.name
-        remainingDistance=get_distance_metres_rover(rover.location.global_relative_frame, targetLocation)
-        print("Distance to target: ", remainingDistance)
-        if remainingDistance<=targetDistance*0.25: #Just below target, in case of undershoot.
-            print("Reached target")
-            break;
-        time.sleep(2)
-        
 arm_and_takeoff(4)
 goto_rover(10,12)
 # swarm code
