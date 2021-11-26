8fd9ffda0491bf624a8f0b7b8b1710f32f5f6337
arrown <80968603+arrown@users.noreply.github.com>
2021-11-26 12:43:53 +0900

    Update roverconnecttest.py

--- roverconnecttest.py
+++ roverconnecttest.py
@@ -11,25 +11,29 @@ time.sleep(1)
 
 def get_location_metres(original_location, dNorth, dEast):
 
-    earth_radius = 6378137.0
+    earth_radius = 6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
     dLat = dNorth/earth_radius
-    dLon = dEast/(earth)Radius*math.com(math.pi*original_locaiton.lat/180))
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
 
-    newlat = original_location.lat +(dLat*180/math.pi)
-    newlon = original_location.lon + (dLon*180/math.pi)
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
     if type(original_location) is LocationGlobal:
-        targetlocation= LocationGlobal(newlat, newlon, original_location.alt)
+        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
     elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon, original_location.alt)
+        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
     else:
         raise Exception("Invalid Location object passed")
-
+        
     return targetlocation;
 
+
 def get_distance_metres(aLocation1, aLocation2):
-    dlat = aLocation2.lat = aLocation1.lat
-    dlong = aLocation2.lon = aLocation1.lon
-    return math.sqrt((dlat*dlat)+(dlong*dlong)) * 1.113195e5
+
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 
 def goto_rover(dNorth, dEast, gotoFunction=rover.simple_goto):
 
