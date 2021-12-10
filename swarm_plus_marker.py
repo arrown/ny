052836d1ad7bb892229f987a7cdfc8cba743fc19
arrown <80968603+arrown@users.noreply.github.com>
2021-12-11 02:01:38 +0900

    Update swarm_plus_marker.py

--- swarm_plus_marker.py
+++ swarm_plus_marker.py
@@ -4,7 +4,6 @@ import cv2.aruco as aruco
 import sys, time, math
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative, Command
 from pymavlink import mavutil
-from opencv.lib_aruco_pose import *
 
 print ("connecting to Drone")
 drone = connect("/dev/ttyACM0", wait_ready = True, timeout = 120, heartbeat_timeout=120)
@@ -78,60 +77,6 @@ def get_distance_metres(aLocation1, aLocation2):
     dlong = aLocation2.lon - aLocation1.lon
     return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 
-def marker_position_to_angle(x, y, z):
-    
-    angle_x = math.atan2(x,z)
-    angle_y = math.atan2(y,z)
-    
-    return (angle_x, angle_y)
-    
-def camera_to_uav(x_cam, y_cam):
-    x_uav =-y_cam
-    y_uav = x_cam
-    return(x_uav, y_uav)
-    
-def uav_to_ne(x_uav, y_uav, yaw_rad):
-    c       = math.cos(yaw_rad)
-    s       = math.sin(yaw_rad)
-    
-    north   = x_uav*c - y_uav*s
-    east    = x_uav*s + y_uav*c 
-    return(north, east)
-    
-def check_angle_descend(angle_x, angle_y, angle_desc):
-    return(math.sqrt(angle_x**2 + angle_y**2) <= angle_desc)
-
-
-#--------------------------------------------------
-#-------------- PARAMETERS  
-#-------------------------------------------------- 
-rad_2_deg   = 180.0/math.pi
-deg_2_rad   = 1.0/rad_2_deg 
-
-#--------------------------------------------------
-#-------------- LANDING MARKER  
-#--------------------------------------------------    
-#--- Define Tag
-id_to_find      = 1
-marker_size     = 20 #- [cm]
-freq_send       = 1 #- Hz
-
-land_alt_cm         = 100
-angle_descend       = 20*deg_2_rad
-land_speed_cms      = 30.0
-
-
-
-#--- Get the camera calibration path
-
-calib_path  = ""
-camera_matrix   = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
-camera_distortion   = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')                                    
-aruco_tracker       = ArucoSingleTracker(id_to_find=id_to_find, marker_size=marker_size, show_video=False, 
-                camera_matrix=camera_matrix, camera_distortion=camera_distortion)
-                
-time_0 = time.time()
-#----------------------Swarm Start --------------------------------------
 
 altitude = 4
 arm_and_takeoff(altitude)
@@ -157,46 +102,4 @@ while True:
             pass
     time.sleep(2)
     
-#------------------------Marker Start------------------------------------ 
-while True:                
 
-    marker_found, x_cm, y_cm, z_cm = aruco_tracker.track(loop=False)
-    if marker_found:
-        x_cm, y_cm          = camera_to_uav(x_cm, y_cm)
-        uav_location        = drone.location.global_relative_frame
-        
-        #-- If high altitude, use baro rather than visual
-        if uav_location.alt >= 5.0:
-            print 
-            z_cm = uav_location.alt*100.0
-            
-        angle_x, angle_y    = marker_position_to_angle(x_cm, y_cm, z_cm)
-
-        
-        if time.time() >= time_0 + 1.0/freq_send:
-            time_0 = time.time()
-            # print ""
-            print " "
-            print "Altitude = %.0fcm"%z_cm
-            print "Marker found x = %5.0f cm  y = %5.0f cm -> angle_x = %5f  angle_y = %5f"%(x_cm, y_cm, angle_x*rad_2_deg, angle_y*rad_2_deg)
-            
-            north, east             = uav_to_ne(x_cm, y_cm, drone.attitude.yaw)
-            print "Marker N = %5.0f cm   E = %5.0f cm   Yaw = %.0f deg"%(north, east, drone.attitude.yaw*rad_2_deg)
-            
-            marker_lat, marker_lon  = get_location_metres(uav_location, north*0.01, east*0.01)  
-            #-- If angle is good, descend
-            if check_angle_descend(angle_x, angle_y, angle_descend):
-                print "Low error: descending"
-                location_marker         = LocationGlobalRelative(marker_lat, marker_lon, uav_location.alt-(land_speed_cms*0.01/freq_send))
-            else:
-                location_marker         = LocationGlobalRelative(marker_lat, marker_lon, uav_location.alt)
-                
-            drone.simple_goto(location_marker)
-            print "UAV Location    Lat = %.7f  Lon = %.7f"%(uav_location.lat, uav_location.lon)
-            print "Commanding to   Lat = %.7f  Lon = %.7f"%(location_marker.lat, location_marker.lon)
-            
-        #--- COmmand to land
-        if z_cm <= land_alt_cm:
-            if drone.mode == "GUIDED":
-                print (" -->>COMMANDING TO LAND<<")
-                drone.mode = "LAND"
