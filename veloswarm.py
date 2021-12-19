3209477dd67fe51106d07c3b399f3a1f6ec5f344
arrown <80968603+arrown@users.noreply.github.com>
2021-12-19 13:02:40 +0900

    Update veloswarm.py

--- veloswarm.py
+++ veloswarm.py
@@ -86,30 +86,27 @@ def goto_position_target_local_ned(north, east, down):
     # send command to vehicle
     rover.send_mavlink(msg)
     
-def send_global_velocity(velocity_x, velocity_y, velocity_z, duration):
+
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
     """
     Move vehicle in direction based on specified velocity vectors.
     """
-    msg = drone.message_factory.set_position_target_global_int_encode(
+    msg = drone.message_factory.set_position_target_local_ned_encode(
         0,       # time_boot_ms (not used)
         0, 0,    # target system, target component
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
         0b0000111111000111, # type_mask (only speeds enabled)
-        0, # lat_int - X Position in WGS84 frame in 1e7 * meters
-        0, # lon_int - Y Position in WGS84 frame in 1e7 * meters
-        0, # alt - Altitude in meters in AMSL altitude(not WGS84 if absolute or relative)
-        # altitude above terrain if GLOBAL_TERRAIN_ALT_INT
-        velocity_x, # X velocity in NED frame in m/s
-        velocity_y, # Y velocity in NED frame in m/s
-        velocity_z, # Z velocity in NED frame in m/s
-        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
         0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+
 
     # send command to vehicle on 1 Hz cycle
     for x in range(0,duration):
         drone.send_mavlink(msg)
         time.sleep(1)
-  
+        
 altitude = 4
 arm_and_takeoff(altitude)
 time.sleep(4)
